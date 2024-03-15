89593a1a9f8bcd441d7be1533caa8c9bb94e1835
Baptiste Blambert Deminière <baptiste.blambert@epitech.eu>
2024-03-15 10:30:08 +0100

    test cd style

--- src/swapupcase.c
+++ src/swapupcase.c
@@ -29,6 +29,7 @@ int swapupcase(char *str)
     res = case_string(str, is_string_odd);
     if (res == ERROR)
         return EPITECH_KO;
+    int z = 0;
     write(1, str, size);
     return EPITECH_OK;
 }
